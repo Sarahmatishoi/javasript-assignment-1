687db789f5e6b65b4bd950524fc24847022ca433
sarahmatishoi <sarahmatishoi@gmail.com>
2021-03-19 19:47:40 +0300

    first commit

---
+++ text.js
@@ -0,0 +1,11 @@
+function name(){
+    var a="shadya";
+    var b="pamera";
+    function identity(){
+        var c= a + " and " + b + " are friends";
+        console.log(c)
+    }
+    identity();
+
+}
+name();
